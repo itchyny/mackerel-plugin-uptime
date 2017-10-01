09460f74b161b9054a258444cc60085ad1370b26
itchyny <itchyny@hatena.ne.jp>
2017-10-01 14:47:37 +0900

    support MacOS and *BSD

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate libc;
 #[macro_use]
 extern crate mackerel_plugin;
+extern crate time;
 
 use std::collections::HashMap;
 use std::mem;
@@ -8,6 +9,7 @@ use mackerel_plugin::*;
 
 pub struct UptimePlugin {}
 
+#[cfg(target_os = "linux")]
 fn get_uptime() -> Result<f64, String> {
     let mut info: libc::sysinfo = unsafe { mem::zeroed() };
     unsafe {
@@ -16,6 +18,28 @@ fn get_uptime() -> Result<f64, String> {
             Ok(info.uptime as f64)
         } else {
             Err("sysinfo failed".to_string())
+        }
+    }
+}
+
+#[cfg(any(target_os = "macos", target_os = "freebsd", target_os = "openbsd", target_os = "netbsd"))]
+fn get_uptime() -> Result<f64, String> {
+    let mut request = [libc::CTL_KERN, libc::KERN_BOOTTIME];
+    let mut boottime: libc::timeval = unsafe { mem::zeroed() };
+    let mut size: libc::size_t = mem::size_of_val(&boottime) as libc::size_t;
+    unsafe {
+        let ret = libc::sysctl(
+            &mut request[0],
+            2,
+            &mut boottime as *mut libc::timeval as *mut libc::c_void,
+            &mut size,
+            std::ptr::null_mut(),
+            0,
+        );
+        if ret == 0 {
+            Ok((time::now().to_timespec().sec - boottime.tv_sec) as f64)
+        } else {
+            Err("sysctl failed".to_string())
         }
     }
 }
